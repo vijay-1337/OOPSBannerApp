3d132b27d330caaa3b1441a382da8fa6705a118e
Vijay Kumar <vijaykumar11052006@gmail.com>
2026-03-01 18:33:12 +0530

    Create string.java

---
+++ string.java
@@ -0,0 +1,13 @@
+public class OOPS_string {
+    public static void main(String[] args) {
+
+        System.out.println(String.join(" ","    ***    ","    ***    "," ****** ", "   ***** "));
+        System.out.println(String.join(" ","  **   **  ","  **   **  "," **   **", " **      "));
+        System.out.println(String.join(" "," **     ** "," **     ** "," **   **", "***      "));
+        System.out.println(String.join(" "," **     ** "," **     ** "," ****** ", "  *****  "));
+        System.out.println(String.join(" "," **     ** "," **     ** "," **     ", "       **"));
+        System.out.println(String.join(" ","  **   **  ","  **   **  "," **     ", "**     **"));
+        System.out.println(String.join(" ","    ***    ","    ***    "," **     ", "  *****  "));
+
+    }
+}
